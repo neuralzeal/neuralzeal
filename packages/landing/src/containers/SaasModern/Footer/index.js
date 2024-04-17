fd2c2c414850049d85bfe697970699b83fa07d62
neuralzeal <neuralzealn@gmail.com>
2024-04-17 15:25:18 +0530

    Updated the contact details

--- packages/landing/src/containers/SaasModern/Footer/index.js
+++ packages/landing/src/containers/SaasModern/Footer/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Link from 'next/link';
-import Box from 'common/components/Box';
-import Text from 'common/components/Text';
-import Heading from 'common/components/Heading';
-import Logo from 'common/components/UIElements/Logo';
-import Container from 'common/components/UI/Container';
-import FooterWrapper, { List, ListItem } from './footer.style';
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import Box from "common/components/Box";
+import Text from "common/components/Text";
+import Heading from "common/components/Heading";
+import Logo from "common/components/UIElements/Logo";
+import Container from "common/components/UI/Container";
+import FooterWrapper, { List, ListItem } from "./footer.style";
 
-import LogoImage from 'common/assets/image/saasModern/logo.png';
+import LogoImage from "common/assets/image/saasModern/logo.png";
 
-import { FOOTER_WIDGET } from 'common/data/SaasModern';
+import { FOOTER_WIDGET } from "common/data/SaasModern";
 
 const Footer = ({
   row,
@@ -32,8 +32,8 @@ const Footer = ({
               title="Hosting"
               logoStyle={logoStyle}
             />
-            <Text content="hello@redq.io" {...textStyle} />
-            <Text content="+479-443-9334" {...textStyle} />
+            <Text content="sales@neuralzeal.com" {...textStyle} />
+            <Text content="+91-7094435544" {...textStyle} />
           </Box>
           {/* End of footer logo column */}
           <Box {...colTwo}>
@@ -75,48 +75,48 @@ Footer.defaultProps = {
   // Footer row default style
   row: {
     flexBox: true,
-    flexWrap: 'wrap',
-    ml: '-15px',
-    mr: '-15px',
+    flexWrap: "wrap",
+    ml: "-15px",
+    mr: "-15px",
   },
   // Footer col one style
   colOne: {
-    width: [1, '35%', '35%', '23%'],
-    mt: [0, '13px'],
-    mb: ['30px', 0],
-    pl: ['15px', 0],
-    pr: ['15px', '15px', 0],
+    width: [1, "35%", "35%", "23%"],
+    mt: [0, "13px"],
+    mb: ["30px", 0],
+    pl: ["15px", 0],
+    pr: ["15px", "15px", 0],
   },
   // Footer col two style
   colTwo: {
-    width: ['100%', '65%', '65%', '77%'],
+    width: ["100%", "65%", "65%", "77%"],
     flexBox: true,
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
   },
   // Footer col default style
   col: {
-    width: ['100%', '50%', '50%', '25%'],
-    pl: '15px',
-    pr: '15px',
-    mb: '30px',
+    width: ["100%", "50%", "50%", "25%"],
+    pl: "15px",
+    pr: "15px",
+    mb: "30px",
   },
   // widget title default style
   titleStyle: {
-    color: '#343d48',
-    fontSize: '16px',
-    fontWeight: '700',
-    mb: '30px',
+    color: "#343d48",
+    fontSize: "16px",
+    fontWeight: "700",
+    mb: "30px",
   },
   // Default logo size
   logoStyle: {
-    width: '130px',
-    mb: '15px',
+    width: "130px",
+    mb: "15px",
   },
   // widget text default style
   textStyle: {
-    color: '#0f2137',
-    fontSize: '16px',
-    mb: '10px',
+    color: "#0f2137",
+    fontSize: "16px",
+    mb: "10px",
   },
 };
 
